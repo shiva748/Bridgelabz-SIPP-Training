93553ee8fd28ea24465dde7cd3ee0b412f6196f9
Shiva <Shivagautam2002@gmail.com>
2025-06-16 09:37:34 +0530

    Refactor : program to check is no is abundant

--- ControlFlow/AbundantNumber.java
+++ ControlFlow/AbundantNumber.java
@@ -2,15 +2,15 @@ import java.util.*;
 
 public class AbundantNumber {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number ");
-        int n = sc.nextInt(), sum = 1;
+        int n = scanner.nextInt(), sum = 1;
         for (int i = 2; i * i < n; i++) {
             if (n % i == 0) {
                 sum += (i + n / i);
             }
         }
         System.out.println(sum > n ? "Abundant Number" : "Not an Abundant Number");
-        sc.close();
+        scanner.close();
     }
 }
