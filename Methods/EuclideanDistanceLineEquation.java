a0b1ad7261ca853d115db1fa80784023ee4d2dcb
Shiva <Shivagautam2002@gmail.com>
2025-06-18 11:13:01 +0530

    Refactor: Remove unnecessary comments and streamline line equation output in EuclideanDistanceLineEquation class

--- Methods/EuclideanDistanceLineEquation.java
+++ Methods/EuclideanDistanceLineEquation.java
@@ -2,13 +2,10 @@ import java.util.Scanner;
 
 public class EuclideanDistanceLineEquation {
 
-    // Method to calculate Euclidean distance between two points
     public static double euclideanDistance(double x1, double y1, double x2, double y2) {
         return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
 
-    // Method to calculate slope and y-intercept of line given two points
-    // Returns array: [slope, yIntercept]
     public static double[] lineEquation(double x1, double y1, double x2, double y2) {
         double slope;
         if (x2 - x1 == 0) {
@@ -38,12 +35,8 @@ public class EuclideanDistanceLineEquation {
         double distance = euclideanDistance(x1, y1, x2, y2);
         System.out.println("Euclidean distance between points: " + distance);
 
-        try {
-            double[] equation = lineEquation(x1, y1, x2, y2);
-            System.out.println("Equation of line: y = " + equation[0] + "x + " + equation[1]);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
+        double[] line = lineEquation(x1, y1, x2, y2);
+        System.out.printf("Line equation: y = %.2fx + %.2f\n", line[0], line[1]);
 
         scanner.close();
     }
