1d6a4de6461086ce28ab4dd32fd89d5bf0f84452
Shiva <Shivagautam2002@gmail.com>
2025-08-14 09:40:53 +0530

    Added : Implementation of Extract Emails

---
+++ Regex_&_unit/MultipleSpaces.java
@@ -0,0 +1,8 @@
+
+public class MultipleSpaces {
+    public static void main(String[] args) {
+        String text = "This   is   an   example    with   multiple   spaces.";
+        String result = text.replaceAll("\\s+", " ");
+        System.out.println(result);
+    }
+}
